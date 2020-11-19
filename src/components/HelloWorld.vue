6028879c419a321c381c5555f403b26c98f6dcc7
Mlxa0324 <33782374+Mlxa0324@users.noreply.github.com>
2020-11-19 09:48:12 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>{{}}</h1>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+          >babel</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+          >eslint</a
+        >
+      </li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+          >Forum</a
+        >
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+          >Community Chat</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+          >Twitter</a
+        >
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      </li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li>
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+          >vue-router</a
+        >
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          target="_blank"
+          rel="noopener"
+          >vue-devtools</a
+        >
+      </li>
+      <li>
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+          >vue-loader</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+          >awesome-vue</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import pathUtils from "./../common/pathUtils";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data: function () {
+    return {
+      url:
+        "https%3A%2F%2Fkefu.wshello.cn%2F%23%2Findex%3Favatar%3Dhttps%3A%2F%2Fimg.wshello.cn%2Fupload%2Fimg%2Fstore%2F0%2F1576120162929.jpg%26uid%3D1092f5d58e8c410890b269ee1924bad3%26nickname%3D%E5%8D%8E%E8%A3%B3%E4%B9%9D%E5%B7%9E%26c%3D1",
+    };
+  },
+  mounted() {
+    console.log(pathUtils.urlParse(this.url));
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
